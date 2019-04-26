7589cc6230c5d8796dfdc5bc9e3687dd510873ac
Sang Le <lesan6021@gmail.com>
2019-04-26 16:46:05 -0400

    Modify route to both POST and GET

--- index.php
+++ index.php
@@ -34,12 +34,12 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET /order', function(){
+$f3->route('GET|POST /order', function(){
     $view = new Template();
     echo $view->render('views/form1.html');
 });
 
-$f3->route('POST /order2', function(){
+$f3->route('POST|GET /order2', function(){
     $view = new Template();
 
     $_SESSION['animal'] = $_POST['animal'];
