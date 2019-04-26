06055f3f9622eee26d2c13d8d6f092ce5420474d
Sang Le <lesan6021@gmail.com>
2019-04-26 18:03:17 -0400

    Add validation for color

--- index.php
+++ index.php
@@ -56,21 +56,25 @@ $f3->route('GET|POST /order', function($f3){
     echo $view->render('views/form1.html');
 });
 
-$f3->route('POST|GET /order2', function(){
+$f3->route('POST|GET /order2', function($f3){
+    if(isset($_POST['color'])) {
+        $color = $_POST['color'];
+
+        if(validColor($color)) {
+            $_SESSION['color'] = $color;
+            $f3->reroute('/results');
+        }
+        else {
+            $f3->set("error['color']", "Not a valid color");
+        }
+    }
+
     $view = new Template();
-
-    $_SESSION['animal'] = $_POST['animal'];
-
-
     echo $view->render('views/form2.html');
 });
 
-$f3->route('POST /results', function(){
+$f3->route('POST|GET /results', function(){
     $view = new Template();
-
-    $_SESSION['color'] = $_POST['color'];
-
-
     echo $view->render('views/results.html');
 });
 
